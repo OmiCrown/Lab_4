5918e63f6f992a3ec0ea4375202d64580b2c0ca3
OmiCrown <omicron@ukr.net>
2013-12-16 00:50:52 +0200

    Создана коза документации класса MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -6,62 +6,126 @@
  */
 import java.util.LinkedList;
 
+/**
+ *
+ * @author Omicrown
+ */
 public class MyLinkedList {
 
     private LinkedList<Integer> ll;
 
+    /**
+     *
+     */
     public MyLinkedList() {
     }
 
+    /**
+     *
+     * @param e
+     */
     public void add(Integer e) {
         ll.add(e);
     }
 
+    /**
+     *
+     * @param e
+     * @param element
+     */
     public void add(Integer e, Integer element) {
         ll.add(e, element);
     }
 
+    /**
+     *
+     * @param e
+     */
     public void addFirst(Integer e) {
         ll.addFirst(e);
     }
 
+    /**
+     *
+     * @param e
+     */
     public void addLast(Integer e) {
         ll.add(e);
     }
 
+    /**
+     *
+     * @param index
+     * @return
+     */
     public Integer get(int index) {
         return ll.get(index);
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getFirst() {
         return ll.getFirst();
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getLast() {
         return ll.getLast();
     }
 
+    /**
+     *
+     * @param index
+     * @return
+     */
     public Integer remove(int index) {
         return ll.remove(index);
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer removeFirst() {
         return ll.removeFirst();
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer removeLast() {
         return ll.removeLast();
     }
 
+    /**
+     *
+     * @param index
+     * @param element
+     */
     public void set(int index, Integer element) {
         Integer el;
         el = ll.set(index, element);
     }
 
+    /**
+     *
+     * @return
+     */
     public int size() {
         return ll.size();
     }
     
+    /**
+     *
+     * @param o
+     * @return
+     */
     public int indexOf(Integer o){
         return ll.indexOf(o);
     }

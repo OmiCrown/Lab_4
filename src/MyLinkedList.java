fa7a70928231bd6787e3ee3de819f97b7d4839e3
OmiCrown <omicron@ukr.net>
2013-12-16 00:47:33 +0200

    Заполнены методы класса MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,58 +1,68 @@
 /**
- *  Desc
+ * Description of the project
+ *
+ * @version 0.03
+ * @author OmiCrown
  */
+import java.util.LinkedList;
+
 public class MyLinkedList {
-    public MyLinkedList() {}
-    
-    public void add(Integer e){
-        
-    }
-    
-    public void add(Integer e, Integer element){
-        
+
+    private LinkedList<Integer> ll;
+
+    public MyLinkedList() {
     }
 
-    public void addFirst(Integer e){
+    public void add(Integer e) {
+        ll.add(e);
+    }
 
+    public void add(Integer e, Integer element) {
+        ll.add(e, element);
+    }
+
+    public void addFirst(Integer e) {
+        ll.addFirst(e);
+    }
+
+    public void addLast(Integer e) {
+        ll.add(e);
+    }
+
+    public Integer get(int index) {
+        return ll.get(index);
+    }
+
+    public Integer getFirst() {
+        return ll.getFirst();
+    }
+
+    public Integer getLast() {
+        return ll.getLast();
+    }
+
+    public Integer remove(int index) {
+        return ll.remove(index);
+    }
+
+    public Integer removeFirst() {
+        return ll.removeFirst();
+    }
+
+    public Integer removeLast() {
+        return ll.removeLast();
+    }
+
+    public void set(int index, Integer element) {
+        Integer el;
+        el = ll.set(index, element);
+    }
+
+    public int size() {
+        return ll.size();
     }
     
-    public void addLast(Integer e){
-        
+    public int indexOf(Integer o){
+        return ll.indexOf(o);
     }
-    
-    public Integer get(int index){
-        
-    }
-    
-    public Integer getFirst(){
-        
-    }
-    
-    public Integer getLast(){
-        
-    }
-    
-    public Integer remove(int index){
-        
-    }
-    
-    public Integer removeFirst(){
-        
-    }
-    
-    public Integer removeLast(){
-        
-    }
-    
-    public void set(int index, Integer element){
-        
-    }
-    
-    public int size(){
-        
-    }
-    
-    
 }
-
-//~ Formatted by Jindent --- http://www.jindent.com

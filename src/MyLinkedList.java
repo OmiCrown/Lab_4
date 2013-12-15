7dc5e345f01d41226c6efae021bfeda340d23d33
OmiCrown <omicron@ukr.net>
2013-12-15 18:59:18 +0200

    Созданы заготовки для методов класса MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,10 +1,58 @@
-
 /**
  *  Desc
  */
 public class MyLinkedList {
     public MyLinkedList() {}
-}
+    
+    public void add(Integer e){
+        
+    }
+    
+    public void add(Integer e, Integer element){
+        
+    }
 
+    public void addFirst(Integer e){
+
+    }
+    
+    public void addLast(Integer e){
+        
+    }
+    
+    public Integer get(int index){
+        
+    }
+    
+    public Integer getFirst(){
+        
+    }
+    
+    public Integer getLast(){
+        
+    }
+    
+    public Integer remove(int index){
+        
+    }
+    
+    public Integer removeFirst(){
+        
+    }
+    
+    public Integer removeLast(){
+        
+    }
+    
+    public void set(int index, Integer element){
+        
+    }
+    
+    public int size(){
+        
+    }
+    
+    
+}
 
 //~ Formatted by Jindent --- http://www.jindent.com

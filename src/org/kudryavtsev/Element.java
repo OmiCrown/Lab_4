d9bbbbb1811ef6584cf09a3a83d7c4a47a66d549
OmiCrown <omicron@ukr.net>
2013-12-16 05:31:27 +0200

    Созданы заготовки методов для класса Element

---
+++ src/org/kudryavtsev/Element.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author Omicrown
+ */
+package org.kudryavtsev;
+
+public class Element {
+    private Element el;
+    
+    public Element(Element e){
+        el = e;
+    }
+    
+    public void offer(Element e){
+        
+    }
+    
+    public Element peek(Element e){
+        return e;
+    }
+    
+    public Element poll(Element e){
+        return e;
+    }
+}
